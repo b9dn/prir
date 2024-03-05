49c050fbbe7a6b234bc8656dd26d5efe0bee2099
maciejas22 <maciejas22@DESKTOP-HD39M9T>
2024-03-05 21:03:16 +0100

    usr1 setup

--- c1/main.c
+++ c1/main.c
@@ -11,8 +11,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-void on_usr1(int signal) {
-    printf("Otrzymałem USR1\n");
+int n, z;
+pid_t* child_pids;
+
+void parent_on_usr1(int signal) {
+    printf("Parent otrzymal USR1\n");
+    for(int i = 0; i < n; i++) {
+        kill(child_pids[i], SIGUSR1);
+    }
+}
+
+void child_on_usr1(int signal) {
+    printf("Child otrzymal USR1\n");
 }
 
 // TAG/folder LAB_1_SIG_MEM  + "_COS"
@@ -20,14 +30,12 @@ int main (int argc, char** argv) {
     printf("Moj PID - %d\n", getpid());
     sigset_t mask; /* Maska sygnałów */
 
-    signal(SIGUSR1, &on_usr1);
-
-    int n = argc < 2 ? 5 : atoi(argv[1]);
-    int z = argc < 3 ? 20 : atoi(argv[2]);
+    n = argc < 2 ? 5 : atoi(argv[1]);
+    z = argc < 3 ? 20 : atoi(argv[2]);
     char* data_filename = "liczby.out";
     char* result_filename = "wynik.out";
     int* primes = generate_primes_list(z);
-    pid_t* child_pids = malloc(n * sizeof(pid_t));
+    child_pids = calloc(n, sizeof(pid_t));
 
     int* result = (int*)create_shared_memory(n * sizeof(int));
     int* range = (int*)create_shared_memory(2 * n * sizeof(int));
@@ -41,12 +49,16 @@ int main (int argc, char** argv) {
                 fprintf(stderr, "Blad w fork\n");
                 return EXIT_FAILURE;
             case 0: {
+                signal(SIGUSR1, &child_on_usr1);
                 sigsuspend(&mask);
                 result[i] = calc_sum_in_range(primes, range[i*2], range[i*2+1]);
                 printf("Zakonczono %d\n", getpid());
                 return EXIT_SUCCESS;
             }
             default:
+                if(i == n-1) {
+                    signal(SIGUSR1, &parent_on_usr1);
+                }
                 continue;
         }
     }
@@ -62,6 +74,7 @@ int main (int argc, char** argv) {
     write_nums_to_file(primes, z, data_filename, "Liczba");
     write_nums_to_file(result, n, result_filename, "Wynik");
 
+    free(child_pids);
     free_primes_list(primes);
     free_shared_memory(range, 2 * n * sizeof(int));
     free_shared_memory(result, n * sizeof(int));
