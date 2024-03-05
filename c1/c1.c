7cac9607f7d9cebd2912720e0a9dcc4f2742394b
maciejas22 <maciejas22@DESKTOP-HD39M9T>
2024-03-05 21:41:04 +0100

    merge all files

--- c1/c1.c
+++ c1/c1.c
@@ -11,26 +11,57 @@
 #include <string.h>
 #include <stdlib.h>
 
-void on_usr1(int signal) {
-    printf("Otrzymałem USR1\n");
-}
+int n, z;
+pid_t* child_pids;
 
-void init_range_and_result(int* range, int* result, int n, int z) {
-    int default_chunk_size = (z + 1) / n;
-    int ptr = 0;
+void parent_on_usr1(int signal) {
+    printf("Parent otrzymal USR1\n");
     for(int i = 0; i < n; i++) {
-        int chunk_size = i == n - 1 ? z - (n-1) * default_chunk_size : default_chunk_size;
-        range[i*2] = ptr;
-        range[i*2+1] = ptr + chunk_size - 1;
-        ptr += chunk_size;
-        printf("%d - %d   chunk_size %d\n", range[i*2], range[i*2+1], chunk_size);
+        kill(child_pids[i], SIGUSR1);
     }
 }
 
-void* create_shared_memory(int size) {
-    int protection = PROT_READ | PROT_WRITE;
-    int visibility = MAP_SHARED | MAP_ANONYMOUS;
-    return mmap(NULL, size, protection, visibility, -1, 0);
+void child_on_usr1(int signal) {
+    printf("Child otrzymal USR1\n");
+}
+
+void calc_range_array(int* arr, int n, int z) {
+    int default_chunk_size = z / n;
+    int reminder = z % n;
+    int ptr = 0;
+    for(int i = 0; i < n; i++) {
+        int chunk_size = reminder-- > 0 ? default_chunk_size + 1 : default_chunk_size;
+        arr[i*2] = ptr;
+        arr[i*2+1] = ptr + chunk_size - 1;
+        ptr += chunk_size;
+        printf("range segment %d: %d - %d\n", i, arr[i*2], arr[i*2+1]);
+    }
+}
+
+int calc_sum_in_range(int* arr, int start, int end) {
+    int sum = 0;
+    for(int i = start; i <= end; i++)
+        sum += arr[i];
+    return sum;
+}
+
+int calc_sum(int* arr, int size) {
+    int sum = 0;
+    for(int i = 0; i < size; i++)
+        sum += arr[i];
+    return sum;
+}
+
+void write_nums_to_file(int *nums, int size, char *filename, char* line_prefix) {
+    FILE *file = fopen(filename, "w");
+    if(file == NULL) {
+        fprintf(stderr, "Blad w fopen");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0; i < size; i++) {
+        fprintf(file, "%s %d: %d\n", line_prefix, i, nums[i]);
+    }
+    fclose(file);
 }
 
 int is_prime(int number) {
@@ -41,11 +72,11 @@ int is_prime(int number) {
     return 1;
 }
 
-int* generate_primes(int z) {
-    int* primes = (int*)calloc(z, sizeof(int));
+int* generate_primes_list(int n) {
+    int* primes = (int*)malloc(n * sizeof(int));
     int ptr = 0;
     int potential_prime = 2;
-    while(ptr < z) {
+    while(ptr < n) {
         if(is_prime(potential_prime)) {
             primes[ptr++] = potential_prime;
         }
@@ -55,31 +86,39 @@ int* generate_primes(int z) {
     return primes;
 }
 
-void sum(int* primes, int* range, int* result, int index) {
-    int sum = 0;
-    for(int i = range[index*2]; i <= range[index*2+1]; i++)
-        sum += primes[i];
+void* create_shared_memory(int size) {
+    int protection = PROT_READ | PROT_WRITE;
+    int visibility = MAP_SHARED | MAP_ANONYMOUS;
+    int *shm_seg = mmap(NULL, size, protection, visibility, -1, 0);
+    if(shm_seg == MAP_FAILED) {
+        fprintf(stderr, "Blad w mmap");
+        exit(EXIT_FAILURE);
+    }
+    return shm_seg;
+}
 
-    result[index] = sum;
+void free_shared_memory(void* ptr, int size) {
+    if(munmap(ptr, size) == -1) {
+        fprintf(stderr, "Blad w munmap");
+        exit(EXIT_FAILURE);
+    }
 }
 
 // TAG/folder LAB_1_SIG_MEM  + "_COS"
 int main (int argc, char** argv) {
     printf("Moj PID - %d\n", getpid());
-    /* Pobierz swój PID */
     sigset_t mask; /* Maska sygnałów */
 
-    signal(SIGUSR1, &on_usr1);
+    n = argc < 2 ? 5 : atoi(argv[1]);
+    z = argc < 3 ? 20 : atoi(argv[2]);
+    char* data_filename = "liczby.out";
+    char* result_filename = "wynik.out";
+    int* primes = generate_primes_list(z);
+    child_pids = calloc(n, sizeof(pid_t));
 
-    int n = argc < 2 ? 5 : atoi(argv[1]);
-    int z = argc < 3 ? 20 : atoi(argv[2]);
-    char* filename = "liczby.out";
-    int* primes = generate_primes(z);
-    pid_t* child_pids = malloc(n * sizeof(pid_t));
-
-    int* range = (int*)create_shared_memory(2 * n * sizeof(int));
     int* result = (int*)create_shared_memory(n * sizeof(int));
-    init_range_and_result(range, result, n, z);
+    int* range = (int*)create_shared_memory(2 * n * sizeof(int));
+    calc_range_array(range, n, z);
     
     sigfillset(&mask);
     sigdelset(&mask, SIGUSR1);
@@ -89,29 +128,34 @@ int main (int argc, char** argv) {
                 fprintf(stderr, "Blad w fork\n");
                 return EXIT_FAILURE;
             case 0: {
+                signal(SIGUSR1, &child_on_usr1);
                 sigsuspend(&mask);
-                sum(primes, range, result, i);
+                result[i] = calc_sum_in_range(primes, range[i*2], range[i*2+1]);
                 printf("Zakonczono %d\n", getpid());
                 return EXIT_SUCCESS;
             }
             default:
+                if(i == n-1) {
+                    signal(SIGUSR1, &parent_on_usr1);
+                }
                 continue;
         }
     }
 
     for (int i = 0; i < n; i++) {
-        printf("child - %d\n", child_pids[i]);
+        printf("child %d pid: %d\n", i, child_pids[i]);
     }
     while(wait(0) != -1) {}
 
-    int sum = 0;
-    for(int i = 0; i < n; i++) {
-        sum += result[i];
-        printf("Result i - %d   =   %d\n", i, result[i]);
-    }
-    printf("Sum = %d\n", sum);
+    int sum = calc_sum(result, n);
+    printf("Suma calkowita = %d\n", sum);
 
+    write_nums_to_file(primes, z, data_filename, "Liczba");
+    write_nums_to_file(result, n, result_filename, "Wynik");
+
+    free(child_pids);
     free(primes);
+    free_shared_memory(range, 2 * n * sizeof(int));
+    free_shared_memory(result, n * sizeof(int));
     return 0;
 }
-
