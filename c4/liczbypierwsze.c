fe83cc17c273c3d3d855e8e93d5e800d94cdd092
maciejas22 <motorosas2012@gmail.com>
2024-05-11 14:04:49 +0200

    remove comments, make malloc safe

--- c4/liczbypierwsze.c
+++ c4/liczbypierwsze.c
@@ -10,73 +10,87 @@
 #define M N / 10
 
 int main(int argc, char **argv) {
-    long int a[S + 1];    /*tablica pomocnicza*/
-    long int pierwsze[M]; /*liczby pierwsze w przedziale 2..N*/
-    /*long int pierwsze_thread_private_arr[omp_get_max_threads()][M];*/
-    long int** pierwsze_thread_private_arr;
-    long i, k, liczba, reszta;
-    long int lpodz;      /* l. podzielnikow*/
-    long int llpier = 0; /*l. liczb pierwszych w tablicy pierwsze*/
-    long int* llpier_thread_private_arr;
-    double czas;         /*zmienna do  mierzenia czasu*/
-    FILE *fp;
-    /*wyznaczanie podzielnikow z przedzialow 2..S*/
+  long int a[S + 1];
+  long int pierwsze[M];
+  long int **pierwsze_thread_private_arr;
+  long i, k, liczba, reszta;
+  long int lpodz;
+  long int llpier = 0;
+  long int *llpier_thread_private_arr;
+  double czas;
+  FILE *fp;
 
-    int max_threads = omp_get_max_threads();
-    pierwsze_thread_private_arr = malloc(max_threads * sizeof(long int*));
-    for (int j = 0; j < max_threads; j++) {
-        pierwsze_thread_private_arr[j] = malloc(M * sizeof(long int));
+  int max_threads = omp_get_max_threads();
+  pierwsze_thread_private_arr = malloc(max_threads * sizeof(long int *));
+  if (pierwsze_thread_private_arr == NULL) {
+    printf("Malloc failed\n");
+    exit(1);
+  }
+  for (int j = 0; j < max_threads; j++) {
+    pierwsze_thread_private_arr[j] = malloc(M * sizeof(long int));
+    if (pierwsze_thread_private_arr[j] == NULL) {
+      printf("Malloc failed\n");
+      exit(1);
     }
+  }
 
-    llpier_thread_private_arr = malloc(max_threads * sizeof(long int));
-    for (int j = 0; j < max_threads; j++) {
-        llpier_thread_private_arr[j] = 0;
-    }
+  llpier_thread_private_arr = malloc(max_threads * sizeof(long int));
+  if (llpier_thread_private_arr == NULL) {
+    printf("Malloc failed\n");
+    exit(1);
+  }
+  for (int j = 0; j < max_threads; j++) {
+    llpier_thread_private_arr[j] = 0;
+  }
 
-    double start = omp_get_wtime();
+  double start = omp_get_wtime();
 #pragma omp parallel for schedule(dynamic) private(i) shared(a)
-    for (i = 2; i <= S; i++)
-        a[i] = 1; /*inicjowanie*/
-    for (i = 2; i <= S; i++)
-        if (a[i] == 1) {
-            pierwsze[llpier++] = i; /*zapamietanie podzielnika*/
-            /*wykreslanie liczb zlozonych bedacych wielokrotnosciami i*/
+  for (i = 2; i <= S; i++)
+    a[i] = 1;
+  for (i = 2; i <= S; i++)
+    if (a[i] == 1) {
+      pierwsze[llpier++] = i;
 #pragma omp parallel for shared(a) private(k)
-            for (k = i + i; k <= S; k += i)
-                a[k] = 0;
-        }
-
-    lpodz = llpier; /*zapamietanie liczby podzielnikow*/
-/*wyznaczanie liczb pierwszych*/
-#pragma omp parallel for private(k, liczba, reszta) shared(pierwsze, pierwsze_thread_private_arr, llpier_thread_private_arr)
-    for (liczba = S + 1; liczba <= N; liczba++) {
-        for (k = 0; k < lpodz; k++) {
-            reszta = (liczba % pierwsze[k]);
-            if (reszta == 0)
-                break; /*liczba zlozona*/
-        }
-        if (reszta != 0) {
-            int thread_num = omp_get_thread_num();
-            pierwsze_thread_private_arr[thread_num][llpier_thread_private_arr[thread_num]++] = liczba;
-        }
-    }
-/*#pragma omp parallel for private(k) shared(llpier_thread_private_arr, pierwsze_thread_private_arr, pierwsze, llpier)*/
-    for (k = 0; k < max_threads; k++) {
-        for (int j = 0; j < llpier_thread_private_arr[k]; j++) {
-/*#pragma omp critical*/
-            pierwsze[llpier++] = pierwsze_thread_private_arr[k][j];
-        }
+      for (k = i + i; k <= S; k += i)
+        a[k] = 0;
     }
 
-    double end = omp_get_wtime();
-    printf("%lf\n", end - start);
-    if ((fp = fopen("primes.txt", "w")) == NULL) {
-        printf("Nie moge otworzyc pliku do zapisu\n");
-        exit(1);
+  lpodz = llpier;
+#pragma omp parallel for private(k, liczba, reszta)                            \
+    shared(pierwsze, pierwsze_thread_private_arr, llpier_thread_private_arr)
+  for (liczba = S + 1; liczba <= N; liczba++) {
+    for (k = 0; k < lpodz; k++) {
+      reszta = (liczba % pierwsze[k]);
+      if (reszta == 0)
+        break;
     }
-    for (i = 0; i < llpier; i++)
-        fprintf(fp, "%ld\n", pierwsze[i]);
-    fclose(fp);
+    if (reszta != 0) {
+      int thread_num = omp_get_thread_num();
+      pierwsze_thread_private_arr[thread_num]
+                                 [llpier_thread_private_arr[thread_num]++] =
+                                     liczba;
+    }
+  }
+  for (k = 0; k < max_threads; k++) {
+    for (int j = 0; j < llpier_thread_private_arr[k]; j++) {
+      pierwsze[llpier++] = pierwsze_thread_private_arr[k][j];
+    }
+  }
 
-    return 0;
+  double end = omp_get_wtime();
+  printf("%lf\n", end - start);
+  if ((fp = fopen("primes.txt", "w")) == NULL) {
+    printf("Nie moge otworzyc pliku do zapisu\n");
+    exit(1);
+  }
+  for (i = 0; i < llpier; i++)
+    fprintf(fp, "%ld\n", pierwsze[i]);
+  fclose(fp);
+
+  for (int j = 0; j < max_threads; j++) {
+    free(pierwsze_thread_private_arr[j]);
+  }
+  free(pierwsze_thread_private_arr);
+
+  return 0;
 }
